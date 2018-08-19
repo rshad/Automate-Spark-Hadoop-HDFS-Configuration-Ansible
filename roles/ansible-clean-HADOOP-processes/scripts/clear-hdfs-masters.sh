0589752206de98fad8e4b3bbcce9025e55e95375
rshad <rashzk95@gmail.com>
2018-08-19 19:43:47 +0000

    roles ansible-clean-HADOOP-processes and ansible-configure-install-Hadoop are repaired and Hadoop is UP but we need to exchange the pub id between hosts

--- roles/ansible-clean-HADOOP-processes/scripts/clear-hdfs-masters.sh
+++ roles/ansible-clean-HADOOP-processes/scripts/clear-hdfs-masters.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-declare -a masters_directories=("/opt/hadoop/logs/" "/home/hadoop/datanode/" "/home/hadoop/namenode/" "/tmp/hadoop-hadoop/dfs/name/" "/tmp/hadoop-hadoop/dfs/namesecondary/" )
+declare -a masters_directories=("/opt/hadoop/logs/" "/home/fedora/datanode/" "/home/fedora/namenode/" "/tmp/hadoop-fedora/*" "/tmp/hadoop*" "/tmp/spark-events" )
 
 echo "Deleting files in the following directories in [masters] ..."
 for directory in ${masters_directories[@]}
@@ -13,6 +13,4 @@ echo "Deleting HDFS and YARN processes ... "
 for pid in $(ps -fe | grep '[p]rocess' | grep -v grep | awk '{print $2}'); do
     echo "Killing $pid ..."
     kill "$pid"
-    echo "Sleeping 10 seconds"
-    sleep 10
 done
--- roles/ansible-clean-HADOOP-processes/scripts/clear-hdfs-slaves.sh
+++ roles/ansible-clean-HADOOP-processes/scripts/clear-hdfs-slaves.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-declare -a slaves_directories=("/opt/hadoop/logs/" "/home/hadoop/datanode/" "/tmp/hadoop-hadoop/" "/tmp/hadoop-hadoop/dfs/name/" "/tmp/hadoop-hadoop/dfs/namesecondary/")
+declare -a slaves_directories=("/opt/hadoop/logs/" "/home/fedora/datanode/" "/home/fedora/namenode/" "/tmp/hadoop-fedora/*" "/tmp/hadoop*" "/tmp/spark-events")
 
-echo "Deleting files in the following directories in [masters] ..."
+echo "Deleting files in the following directories in [slaves] ..."
 for directory in ${slaves_directories[@]}
 do
      echo "=>" $directory
@@ -13,6 +13,5 @@ echo "Deleting HDFS and YARN processes ... "
 for pid in $(ps -ef | grep hadoop | grep -v grep | awk '{print $2}'); do
     echo "Killing $pid ..."
     kill "$pid"
-    echo "Sleeping 10 seconds"
-    sleep 10
 done
+
